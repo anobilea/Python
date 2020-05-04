b2ed04852c439afb42b04a6ae65ccfe2f75cbd6e
Andres <aperez@gmx.ch>
2020-05-04 19:20:50 +0200

    remove test code form main app

--- calculator/program.py
+++ calculator/program.py
@@ -1,12 +1,8 @@
 import runner
 
-filename = r"/Users/aperez/Documents/programming/Python/input.txt"
+filename = r"enter full filename here"
 result = runner.calculate_file(filename)
 for v, r in result.items():
     print(f"{v} = {r}")
 
 
-print("Proof")
-pi = 3.14
-print(f"big = {18456 / 20 * 3 + 100 * (8456 - 10000 / pi) + 878 ** 15}")
-print(f"floats = {1.10e-10 * 25.3e5 + pi * 0.15}")
